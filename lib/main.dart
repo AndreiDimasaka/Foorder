7072b9a521a1ee238d49675ce96104c8bcb3b4da
Andrei <andreidimasaka@gmail.com>
2026-03-08 16:00:09 +0800

    Made a picture touch gesture widget, to put the pictures, contains the on tap function for it to navigate to food details whenever it is pressed.

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,6 @@ class Cake
 }
 
 
-
 void main()
 {
     runApp(const MyApp());
@@ -91,19 +90,119 @@ void main()
 class MyApp extends StatelessWidget
 {
     const MyApp({super.key});
-
     @override
     Widget build(BuildContext context)
     {
-        return MaterialApp();
+        return MaterialApp(
+            home: Scaffold(
+                body: Dashboard()
+            )
+        );
     }
 }
 
 
-
-
-
 class Dashboard extends StatefulWidget
 {
+    const Dashboard({super.key});
+
+    @override
+    State<Dashboard> createState() => DashboardState();
+}
+
+
+class DashboardState extends State<Dashboard>
+{
+    @override
+    Widget build(BuildContext context)
+    {
+        return 
+        Padding(
+            padding: EdgeInsetsGeometry.all(16),
+            child:
+            Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                spacing: 10.0,
+                children: [
+                    Flexible(
+                        fit: FlexFit.loose,
+                        child:
+                        Container(
+                            height: 300,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Color.fromARGB(255, 128, 128, 128)
+                            ),
+                        )
+                    ),
+                    Row(
+                        children: [
+                            Expanded(
+                                child:
+                                Container(
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Color.fromRGBO(0, 255, 255, 100)
+                                    ),
+                                )
+                            )
+                        ]
+                    ),
+                    Flexible(
+                        fit: FlexFit.loose,
+                        child:
+                        GridView.count(
+                            primary: false,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                            crossAxisCount: 2,
+                            children: [
+
+
+                            ],
+                        )
+                    )
+                ]
+            )
+        );
+
+    }
+}
+
+class PictureButton extends StatelessWidget
+{
+    final String imagePath;
+
+    const PictureButton({super.key, required this.imagePath});
+
+    @override
+    Widget build(BuildContext context)
+    {
+        return GestureDetector(
+            onTap: () => print("Tapped"), //Implement onTap function to transfer to another widget or class
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 5,
+                children: [
+                    Flexible(
+                        fit: FlexFit.loose,
+                        child: Container(
+                            decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                    image: AssetImage(imagePath),
+                                    fit: BoxFit.cover
+                                )
+                            )
+                        ),
+                    ),
+                ]
+            )
+        );
+    }
+
 
 }
